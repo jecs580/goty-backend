c491237f6962aae8008cdb6aaa87f74080d160d0
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-01-28 21:22:24 -0400

    Configuracion de las credenciales del proyecto

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,12 @@
 import * as functions from 'firebase-functions';
 
+import  * as admin from 'firebase-admin';
+
+const serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  credential:admin.credential.cert(serviceAccount)
+}
+)
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
