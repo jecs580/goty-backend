19df0c33343f9cc5baef4c0b437b3719bc63c348
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-01-29 23:46:21 -0400

    Creacion de las rutas para obtener los juegos y para votar por un juego mediante express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,14 @@
 import * as functions from 'firebase-functions';
-
 import  * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
 
 const serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
   credential:admin.credential.cert(serviceAccount)
-}
-)
+});
+
+const db = admin.firestore();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -16,3 +18,44 @@ export const helloWorld = functions.https.onRequest((request, response) => {
       mensaje:'Hello from Firebase!!!'
   });
 });
+export const gotyRef = functions.https.onRequest(async (request, response) => {
+  const gotyRef = db.collection('goty');
+  const docsSnap = await gotyRef.get();
+  const juegos =  docsSnap.docs.map(docs => docs.data());
+  response.status(200).json(juegos);
+});
+
+// Express
+const app = express();
+
+app.use(cors({origin:true}));
+
+app.get('/goty',async(req,res)=>{
+  const gotyRef = db.collection('goty');
+  const docsSnap = await gotyRef.get();
+  const juegos =  docsSnap.docs.map(docs => docs.data());
+  res.status(200).json(juegos);
+});
+
+app.put('/goty/:id',async(req,res)=>{
+   const id = req.params['id'];
+   const gotyRef = db.collection('goty').doc(id);
+   const gameSnap = await gotyRef.get();
+   if(!gameSnap.exists){
+     res.status(404).json({
+       ok:false,
+       mensaje:'No existe un juego con ese id'+id
+     })
+   }else{
+     const before = gameSnap.data()||{votos:0};
+     await gotyRef.update({
+       votos: before.votos+1
+     });
+     res.json({
+       ok:true,
+       mensaje:`Gracias por tu voto a ${before.name}`
+     });
+   }
+});
+
+export const api = functions.https.onRequest(app);
